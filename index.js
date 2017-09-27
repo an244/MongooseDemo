b680b0533c0feb2c1b9c2544f7c06b698558e878
an244 <thienan.le244@gmail.com>
2017-09-27 12:08:11 +0700

    init Server and Mongoose

---
+++ index.js
@@ -0,0 +1,15 @@
+//server express
+const express = require('express');
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views','./views');
+app.use(express.static('public'));
+
+//Mongoose
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const uri = 'mongodb://localhost/shopMongoose';
+mongoose.connect(uri,{useMongoClient:true});
+mongoose.connection
+.once('open', ()=> {app.listen(3000,()=>console.log('Server started'))})
+.on('error', console.error.bind(console, 'Warning Error:'));
